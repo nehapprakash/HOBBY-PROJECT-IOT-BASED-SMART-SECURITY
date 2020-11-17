5e1c7b198aa3bf4ef54ebe686b43ac1f6800ac6b
nehapprakash <50229821+nehapprakash@users.noreply.github.com>
2020-11-17 19:59:07 +0530

    Add files via upload

---
+++ IR.ino
@@ -0,0 +1,34 @@
+const int Proxsensor=13;
+int inputval=0;
+int led=8;
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(led,OUTPUT);
+  pinMode(Proxsensor,INPUT);
+  Serial.begin(9600);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+  if(digitalRead(Proxsensor)==HIGH)
+  {
+    
+    digitalWrite(13,HIGH);
+   
+  }
+  // put your main code here, to run repeatedly:
+ else
+ {
+  digitalWrite(8,HIGH);
+  
+ }
+  inputval=digitalRead(Proxsensor);
+  Serial.println(inputval);
+  delay(1000);
+  
+
+}
---
+++ UR.ino
@@ -0,0 +1,35 @@
+int trig=10;
+int echo=9;
+int buzzer=13;
+
+void setup()
+{
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(buzzer,OUTPUT);
+  Serial.begin(9600);
+  
+}
+void loop()
+{ 
+  long distance,duration;
+  digitalWrite(trig,0);
+  delayMicroseconds(2);
+  digitalWrite(trig,1);
+  delayMicroseconds(10);
+  digitalWrite(trig,0);
+  duration = pulseIn(echo,1);
+  distance = (duration/2)/29.1;
+  Serial.println(distance);
+  delay(500);
+  if(distance<10)
+  {
+    Serial.println("distance is less than 10");
+    digitalWrite(buzzer,1);
+    }
+  else
+   {
+       digitalWrite(buzzer,0);
+    }
+}
+
